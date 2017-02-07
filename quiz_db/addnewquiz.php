a62ad16d357a01127f700f93fdd9bd0ce1a312a9
Richard Zilahi <zilahi@gmail.com>
2017-02-07 09:07:51 +0200

    add new wuiz fix.

--- quiz_db/addnewquiz.php
+++ quiz_db/addnewquiz.php
@@ -1,6 +1,25 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once("MySQL.php");
+MySQL::connect();
 
 $selectedQuestions = $_POST['selectedquestions'];
+$deadline = "2017-12-21";
+$length = count($selectedQuestions);
+$quizName = "lofasz";
+
+$question = '';
+
+for ($i=0; $i<count($selectedQuestions); $i++) {
+  $question .= $selectedQuestions[$i].",";
+}
+
+$insertArray = array('name' => $quizName, 'deadline' => $deadline, 'questions' => $question );
+
+MySQL::insertIntoGroup('`quizs`', $insertArray);
 
 echo json_encode($selectedQuestions);
 
