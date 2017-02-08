c277949006bd8df67dbd2469464304f9517a03f2
Richard Zilahi <zilahi@gmail.com>
2017-02-08 18:26:06 +0200

    noti is not needed at editquestion.js anymore.

--- quiz_db/editquestion.js
+++ quiz_db/editquestion.js
@@ -11,18 +11,6 @@ $(document).ready(function() {
             dataType: "json", // Data type, HTML, json etc.
             data: deleteData, //Form variables
             success: function(response) {
-              var n = noty({
-                text: "Question has been deleted",
-                theme: 'relax',
-                type: 'information',
-                timeout: '5000',
-                  animation: {
-                      open: {height: 'toggle'},
-                      close: {height: 'toggle'},
-                      easing: 'swing',
-                      speed: 500 // opening & closing animation speed
-                  }
-              });
               window.document.location = "questions.php?action=deleted";
             },
             error: function(xhr, ajaxOptions, thrownError) {
