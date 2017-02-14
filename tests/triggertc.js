756f71996b00791ebbe38733ed8b53c70ae17602
Richard Zilahi <zilahi@gmail.com>
2017-02-14 20:22:48 +0200

    implementing triggering test status

---
+++ tests/triggertc.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+  $("#hitme").click(function (e) {
+    e.preventDefault();
+    //alert("clicked");
+    jQuery.ajax({
+        type: "POST",
+        url: "http://robot:bd6259f0769976dc672fe22d30cb174d@http://kat.inf.elte.hu/jenkins/job/robot-demo/build?token=m2L5t8jlA0r880pjzQIrxGn9L6pzg410", //Where to make Ajax calls
+        dataType:"html",
+        success:function(response){
+            //build has started successfully
+    getLastBuild();
+        },
+        error:function (xhr, ajaxOptions, thrownError){
+            alert(thrownError);
+        }
+    });
+  });
+
+  function getConsoleText () {
+
+    jQuery.ajax({
+        type: "POST",
+        url: "http://robot:bd6259f0769976dc672fe22d30cb174d@http://kat.inf.elte.hu/jenkins/job/robot-demo/lastBuild/consoleText",
+        dataType:"html",
+        success:function(response){
+            $("#loading").toggleClass("hidden");
+            $("#buildconsoletext").append(response);
+            console.log(response);
+            // regex for the json
+            var regex = "(\*{3})(.*)(\*{3})";
+
+        },
+        error:function (xhr, ajaxOptions, thrownError){
+            alert(thrownError);
+        }
+    });
+
+  }
+
+  // getting back the last result
+  $(".getlastbuild").click(function (e) {
+    getConsoleText();
+  });
+
+
+  function getLastBuild () {
+    jQuery.ajax({
+        type: "POST",
+        url: "http://robot:bd6259f0769976dc672fe22d30cb174d@http://kat.inf.elte.hu/jenkins/job/robot-demo/lastBuild/api/json?depth=1",
+        dataType:"json",
+        success:function(response){
+            if (response.building) {
+      $("#buildinprogress").removeClass("hidden");
+      setTimeout(getLastBuild, 5000);
+    } else {
+      getConsoleText();
+    }
+        },
+        error:function (xhr, ajaxOptions, thrownError){
+            alert(thrownError);
+        }
+    });
+  }
+
+});
