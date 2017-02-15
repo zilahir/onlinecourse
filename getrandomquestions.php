fcc8f92cb8449a3360687d2f7ca817ee7d5e50d9
Richard Zilahi <zilahi@gmail.com>
2017-02-16 00:28:24 +0200

    implementing generating random quiz.

--- getrandomquestions.php
+++ getrandomquestions.php
@@ -2,7 +2,8 @@
 
 include_once("functions.php");
 
-//
+session_start();
+$loggedInUserName = $_SESSION['username'];
 
 $startsFrom = $_POST["startsFrom"];
 $deadline = $_POST["deadline"];
@@ -10,6 +11,11 @@ $quizname = $_POST["quizname"];
 $limit = $_POST["limit"];
 
 $randomQuestions = getRandomQuestions("10");
+
+
+$insertArray = array('name' => $quizname, 'deadline' => $deadline, 'starts_from' => $startsFrom, 'max_sub' => $limit, 'questions' => $randomQuestions, 'owner' => $loggedInUserName, );
+
+MySQL::insertIntoGroup('`quizs`', $insertArray);
 
 echo json_encode($randomQuestions)
 
