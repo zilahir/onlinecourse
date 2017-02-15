f5602e7bc0b5131747fe634dab270526bfb68237
Richard Zilahi <zilahi@gmail.com>
2017-02-16 00:20:19 +0200

    implementing generating random quiz.

--- getrandomquestions.php
+++ getrandomquestions.php
@@ -2,6 +2,13 @@
 
 include_once("functions.php");
 
+//
+
+$startsFrom = $_POST["startsFrom"];
+$deadline = $_POST["deadline"];
+$quizname = $_POST["quizname"];
+$limit = $_POST["limit"];
+
 $randomQuestions = getRandomQuestions("10");
 
 echo json_encode($randomQuestions)
--- randomgen.js
+++ randomgen.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
   $("#randomquiz").click(function(e) {
     e.preventDefault();
+    var startsFrom = $("#opendate").val();
+    var deadline = $("#deadline").val();
+    var quizname = $("#quizname").val();
+    var limit = $("#submission_limit").val();
+    var quizData = {
+      startsFrom : startsFrom,
+      deadline : deadline,
+      quizname : quizname,
+      limit : limit
+    }
   jQuery.ajax({
           type: "POST", // HTTP method POST or GET
           url: "getrandomquestions.php", //Where to make Ajax calls
-          //dataType:"json", // Data type, HTML, json etc.
-          //data:answerData, //Form variables
+          dataType:"json", // Data type, HTML, json etc.
+          data:quizData, //Form variables
           success:function(response){
-            console.log(response);
+            console.log(quizData);
           },
           error:function (xhr, ajaxOptions, thrownError){
               alert(thrownError);
