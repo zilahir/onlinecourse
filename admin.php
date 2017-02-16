7a7ae3e180e9687c6caf2e1d87aed3cb7b74ffb4
Richard Zilahi <zilahi@gmail.com>
2017-02-16 15:27:00 +0200

    adding quiz results to admin page.

--- admin.php
+++ admin.php
@@ -28,10 +28,27 @@
 
 <div class="row">
   <div class="col-lg-12">
-    <div class="panel panel-default">
-<div class="panel-heading">Quizzes submissions <i class="fa fa-check-square-o"></i></div>
-TODO: show quiz results here.
+    <div class="col-lg-6">
+      <div class="panel panel-default">
+  <div class="panel-heading">Results <i class="fa fa-area-chart"></i> <i class="fa fa-long-arrow-up"></i></div>
+  <div class="list-group">
+  <a href="#" class="list-group-item">
+    <h4 class="list-group-item-heading">List group item heading</h4>
+    <p class="list-group-item-text">...</p>
+  </a>
 </div>
+  </div>
+    </div>
+    <div class="col-lg-6">
+      <div class="panel panel-default">
+  <div class="panel-heading">Results <i class="fa fa-area-chart"></i> <i class="fa fa-long-arrow-down"></i></div>
+  <div class="list-group">
+  <a href="#" class="list-group-item">
+    <h4 class="list-group-item-heading">List group item heading</h4>
+    <p class="list-group-item-text">...</p>
+  </a>
+  </div>
+  </div>
   </div>
 </div>
 
