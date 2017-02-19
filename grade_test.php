82da1e0490b17162466bb4563faf9d9e7c4aa7fe
Richard Zilahi <zilahi@gmail.com>
2017-02-19 16:54:41 +0200

    feedbacking the resul.

--- grade_test.php
+++ grade_test.php
@@ -7,18 +7,31 @@ include_once("functions.php");
 $givenanswers = $_POST['answerData'];
 
 //var_dump ($_POST['answerData']);
-
+$result["answers"] = array();
 $countResults = count($givenanswers);
 
 for ($i=0; $i<$countResults; $i++) {
+    $cpunt = $i+1;
     $questionid = $givenanswers[$i]["questionid"];
     $givenanswerid = $givenanswers[$i]["givenanswer"];
     $result[$questionid] = CheckIfAnswerWasCorrect ($questionid, $givenanswerid);
-
+    $currentAnswer = CheckIfAnswerWasCorrect ($questionid, $givenanswerid);
+    array_push($result["answers"], $currentAnswer);
 }
 
-//var_dump($result);
+//var_dump($result["answers"]);
 
-echo json_encode($result);
+foreach ($result['answers'] as $key => $value) {
+if (true === $value) {
+  $goodAnswers++;
+} else {
+  $badAnswers++;
+}
+}
+
+echo "goodAnswers: ".$goodAnswers.", badAnswers: ".$badAnswers;
+
+
+//echo json_encode($result);
 
 ?>
