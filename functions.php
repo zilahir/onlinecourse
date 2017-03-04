e6c08de22b992740ad42c4a8f3979dfad8bad5e1
Richard Zilahi <zilahi@gmail.com>
2017-03-04 23:42:24 +0200

    user page - result show minimum requirements.

--- functions.php
+++ functions.php
@@ -770,7 +770,7 @@ function checkIfTheresOpenExercises ($method="user") {
             $progressDiv = '
             <div class="progress">
               <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: '.$percent.'%">
-                <span class="sr-only">40% Complete (success)</span>
+                <div style="left:50%" class="required-points"></div>
               </div>
             </div>';
             //$addBorderClass = 'exercise-done';
