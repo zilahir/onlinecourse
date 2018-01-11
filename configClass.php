33ba771c8a1f2231052d547baa9744fef5fb0ec0
Richard Zilahi <rzilahi@Richards-MBP.home>
2018-01-11 15:45:08 +0200

    gitignore.

--- configClass.php
+++ configClass.php
@@ -3,6 +3,6 @@ return array (
     'mysql_host' => 'localhost',
     'mysql_username' => 'root',
     'mysql_password' => 'root',
-    'mysql_database' => 'quiz',
+    'mysql_database' => 'onlinecourse',
 );
 ?>
