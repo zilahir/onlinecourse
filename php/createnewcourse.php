f39052073a44f5ac0cae92758be9744d5a35a09b
Richard Zilahi <rzilahi@Richards-MBP.home>
2018-01-11 22:18:26 +0200

    addign ew course is now working.

--- php/createnewcourse.php
+++ php/createnewcourse.php
@@ -11,9 +11,13 @@ $newCourseCode = $_POST['newCourseCode'];
 $newCourseDesc = $_POST['newCourseDesc'];
 $newCourseIsOpen = $_POST['newCourseIsOpen'];
 
+if ($newCourseIsOpen == "true") {
+  $newCourseIsOpen = true;
+}
+
 $newCourseObject = array('course_code' => $newCourseCode, 'course_name' => $newCourseName, 'course_desc' => $newCourseDesc, 'responsible_teacher' => $_SESSION['user_id'], 'is_open' => $newCourseIsOpen);
 
-//MySQL::insertIntoGroup('`courses`', $newCourseObject);
+MySQL::insertIntoGroup('`courses`', $newCourseObject);
 
 echo json_encode($newCourseObject);
 
