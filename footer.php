8e1a3d52e8f8cf8d9ef9771c1f67e1361ded7528
Richard Zilahi <rzilahi@Richards-MBP.home>
2018-01-12 12:40:14 +0200

    version and footer updated.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <nav id="nav-header" class="navbar navbar-default navbar-fixed-bottom">
   <div class="container">
       <p class="navbar-text">
-        ELTE – IK | Webfejlesztés 1. | <?php getVersionNumber() ?>
+        ELTE – Online coure system | <?php getVersionNumber() ?>
       </p>
     </div>
 </nav>
